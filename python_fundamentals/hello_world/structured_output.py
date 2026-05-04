4ad49bb7603ece221798de9fcd83c43a1a2d6d76
Guillaume Vaudet <guillaumevaudet.pro@gmail.com>
2026-05-04 14:58:49 +0200

    Add python fundamentals with README files

---
+++ python_fundamentals/hello_world/structured_output.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+language = "Python"
+version = 3
+my_pi = 3.1415
+bool_test = 1 == 1
+
+print(f"Language: {language}")
+print("Version:", version)
+print("Pi approx: %.2f" % my_pi)
+print("Computation valid:", bool_test)
