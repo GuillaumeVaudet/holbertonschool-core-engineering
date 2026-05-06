9ee9a51299c97c9f061f032c47ed6b4b7677990f
Guillaume Vaudet <guillaumevaudet.pro@gmail.com>
2026-05-06 15:25:49 +0200

    Remove all ord() using

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 
 for i in range(97, 123):
-    if i != ord('e') and i != ord('q'):
+    if i != 101 and i != 113:
         print("{}".format(chr(i)), end="")
-    i += 1
-
 print()
