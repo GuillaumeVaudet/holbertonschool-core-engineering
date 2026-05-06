aba47e3bd32a64c7eac1df692fa58fbb31d88a10
Guillaume Vaudet <guillaumevaudet.pro@gmail.com>
2026-05-06 15:23:01 +0200

    Try to use a for loop and range function to match with the school checker

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 
-i = ord('a')
-
-while (i <= ord('z')):
-    if (i != ord('e') and i != ord('q')):
+for i in range(97, 123):
+    if i != ord('e') and i != ord('q'):
         print("{}".format(chr(i)), end="")
     i += 1
 
