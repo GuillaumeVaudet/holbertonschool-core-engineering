36fb53f1ae7630363b1d18a7a370f3ca7c939f75
Guillaume Vaudet <guillaumevaudet.pro@gmail.com>
2026-05-08 17:57:15 +0200

    remove all print function to use only one

--- python_fundamentals/functions_modules/uppercase.py
+++ python_fundamentals/functions_modules/uppercase.py
@@ -2,11 +2,10 @@
 
 def uppercase(str):
     """Prints str in uppercase followed by a new line"""
+    string_to_print = ""
     for c in str:
-        if ord(c) >= ord('A') and ord(c) <= ord('Z'):
-            print(c, end="")
-        elif ord(c) >= ord('a') and ord(c) <= ord('z'):
-            print("{}".format(chr(ord(c) - 32)), end="")
+        if ord(c) >= ord('a') and ord(c) <= ord('z'):
+            string_to_print += chr(ord(c) - 32)
         else:
-            print(c, end="")
-    print()
+            string_to_print += c
+    print("{}".format(string_to_print))
