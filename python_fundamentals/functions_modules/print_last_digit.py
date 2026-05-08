292a09131f86ead227f8a1bf2d0c9e66728c33ff
Guillaume Vaudet <guillaumevaudet.pro@gmail.com>
2026-05-08 18:26:23 +0200

    Refactor script and use abs() instead of condition

--- python_fundamentals/functions_modules/print_last_digit.py
+++ python_fundamentals/functions_modules/print_last_digit.py
@@ -2,10 +2,8 @@
 
 def print_last_digit(number):
     """Print the last digit of number"""
-
-    if number < 0:
-        number = number * -1
     
-    last_digit = number % 10
+    last_digit = abs(number) % 10
     print("{}".format(last_digit))
+
     return last_digit
