08cd5937c5db751a422f975105e76208f893cd30
Guillaume Vaudet <guillaumevaudet.pro@gmail.com>
2026-05-08 18:29:21 +0200

    Add end parameter to print function

--- python_fundamentals/functions_modules/print_last_digit.py
+++ python_fundamentals/functions_modules/print_last_digit.py
@@ -4,6 +4,6 @@ def print_last_digit(number):
     """Print the last digit of number"""
 
     last_digit = abs(number) % 10
-    print("{}".format(last_digit))
+    print("{}".format(last_digit), end="")
 
     return last_digit
