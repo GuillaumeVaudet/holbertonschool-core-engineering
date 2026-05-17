2e7c153830e8915d7433605f67e63f6ec553f174
Guillaume Vaudet <guillaumevaudet.pro@gmail.com>
2026-05-17 22:22:26 +0200

    Add 1-rectangle script for inheritance project

---
+++ python_oop/inheritance/1-rectangle.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""First class for inheritance and polymorphism introduction"""
+
+BaseGeometry = __import__('base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """This class represent a Rectangle inherit from BaseGeometry class"""
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+        self.__width = width
+        self.__height = height
+
+
+if __name__ == "__main__":
+    r = Rectangle(3, 5)
+
+    print(r)
+    print(dir(r))
+
+    try:
+        print("Rectangle: {} - {}".format(r.width, r.height))
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
+
+    try:
+        r2 = Rectangle(4, True)
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
