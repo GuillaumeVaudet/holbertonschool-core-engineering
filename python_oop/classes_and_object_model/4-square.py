8505384d52307f06e931d6215e0d880be96a04aa
guillaume vaudet <guillaumevaudet.pro@gmail.com>
2026-05-17 18:16:34 +0200

    Refactor 4-square.py to centralize the validation logic

--- python_oop/classes_and_object_model/4-square.py
+++ python_oop/classes_and_object_model/4-square.py
@@ -5,11 +5,7 @@
 class Square:
     """Class to represent a square"""
     def __init__(self, size=0):
-        if not isinstance(size, int):
-            raise TypeError('size must be an integer')
-        if size < 0:
-            raise ValueError('size must be >= 0')
-        self.__size = size
+        self.size = size
 
     def area(self):
         return self.__size * self.__size
