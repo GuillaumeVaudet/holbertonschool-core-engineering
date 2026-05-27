0f624aba193fc75cab97b9d6bafe448ef3f46641
guillaume vaudet <guillaumevaudet.pro@gmail.com>
2026-05-27 10:56:49 +0200

    fix: remove testing guard

--- python_advanced/file_handling/read_file.py
+++ python_advanced/file_handling/read_file.py
@@ -6,7 +6,3 @@ def read_file(filename=""):
     """Function that reads an utf-8 text file"""
     with open(filename, 'r', encoding="utf-8") as file:
         print(file.read())
-
-
-if __name__ == "__main__":
-    read_file("my_file_0.txt")
