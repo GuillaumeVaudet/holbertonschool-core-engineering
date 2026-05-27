dc8d76488b1d4f3772b5e266b9d08a1f77c94db9
guillaume vaudet <guillaumevaudet.pro@gmail.com>
2026-05-27 10:50:00 +0200

    fix: add docstring to read_file function and make it compliant with pycodestyle

--- python_advanced/file_handling/read_file.py
+++ python_advanced/file_handling/read_file.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
 def read_file(filename=""):
+    """Function that reads an utf-8 text file"""
     with open(filename, encoding="utf-8") as file:
         print(file.read())
+
 
 if __name__ == "__main__":
     read_file("my_file_0.txt")
