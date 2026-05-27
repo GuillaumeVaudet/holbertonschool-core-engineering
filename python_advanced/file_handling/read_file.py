2c7994a085392019cf2de09c7f7b2201405e9bc2
guillaume vaudet <guillaumevaudet.pro@gmail.com>
2026-05-27 10:52:04 +0200

    fix: add reading mode for open function into read_file.py

--- python_advanced/file_handling/read_file.py
+++ python_advanced/file_handling/read_file.py
@@ -2,7 +2,7 @@
 
 def read_file(filename=""):
     """Function that reads an utf-8 text file"""
-    with open(filename, encoding="utf-8") as file:
+    with open(filename, 'r', encoding="utf-8") as file:
         print(file.read())
 
 
